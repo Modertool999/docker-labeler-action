c25df7afb5fa1ed1d20e2f8f556cccb1bb41d3ac
Modi Goldstein-Rosenfeld <modertool999@gmail.com>
2025-06-25 12:26:01 -0400

    edit comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-CMD_NAME=$(basename "$0")                     # "docker" or "docker-buildx"
+CMD_NAME=$(basename "$0") # "docker" or "docker-buildx"
 REAL_PATH="$(command -v "$CMD_NAME")-original"
 
 if [[ ! -x "$REAL_PATH" ]]; then
@@ -18,11 +18,11 @@ elif [[ "$CMD_NAME" == "docker" && "$1" == "build" ]]; then
 fi
 
 if [[ "$INJECT" != true ]]; then
-  # Not a build command → just run Docker as normal
+  # If input is not a build/buildx command then just run docker as normal
   exec "$REAL_PATH" "$@"
 fi
 
-# It’s a build/buildx command → collect labels
+# If it is a build/buildx command, then collect labels
 LABEL_ARGS=()
 while IFS='=' read -r var val; do
   if [[ "$var" =~ ^GITHUB_ ]]; then
